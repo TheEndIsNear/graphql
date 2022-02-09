cf54415bd925198d5fead8a79c5c805260ffac25
Matt Willy <matt.willy@gmail.com>
2022-02-09 10:51:13 -0700

    move code into a seperate resolver module

---
+++ test/plate_slate_web/schema/query/menu_items_test.exs
@@ -0,0 +1,82 @@
+defmodule PlateSlateWeb.Schema.Query.MenuItemsTest do
+  use PlateSlateWeb.ConnCase, async: true
+
+  setup do
+    PlateSlate.Seeds.run()
+  end
+
+  @query """
+  {
+    menuItems {
+      name
+    }
+  }
+  """
+
+  test "menuItems field returns menu items" do
+    conn = build_conn()
+    conn = get(conn, "/api", query: @query)
+
+    assert json_response(conn, 200) ==
+             %{
+               "data" => %{
+                 "menuItems" => [
+                   %{"name" => "Reuben"},
+                   %{"name" => "Croque Monsieur"},
+                   %{"name" => "Muffuletta"},
+                   %{"name" => "Bánh mì"},
+                   %{"name" => "Vada Pav"},
+                   %{"name" => "French Fries"},
+                   %{"name" => "Papadum"},
+                   %{"name" => "Pasta Salad"},
+                   %{"name" => "Water"},
+                   %{"name" => "Soft Drink"},
+                   %{"name" => "Lemonade"},
+                   %{"name" => "Masala Chai"},
+                   %{"name" => "Vanilla Milkshake"},
+                   %{"name" => "Chocolate Milkshake"}
+                 ]
+               }
+             }
+  end
+
+  @query """
+  {
+    menuItems(matching: "reu") {
+      name
+    }
+  }
+  """
+
+  test "menuItems field returns menu items filtered by name" do
+    response = get(build_conn(), "/api", query: @query)
+
+    assert json_response(response, 200) == %{
+             "data" => %{
+               "menuItems" => [
+                 %{"name" => "Reuben"}
+               ]
+             }
+           }
+  end
+
+  @query """
+  {
+    menuItems(matching: 123) {
+      name
+    }
+  }
+  """
+
+  test "menuItems field returns errors when using a bad value" do
+    response = get(build_conn(), "/api", query: @query)
+
+    assert %{
+             "errors" => [
+               %{"message" => message}
+             ]
+           } = json_response(response, 200)
+
+    assert message == "Argument \"matching\" has invalid value 123."
+  end
+end
