b9fd59ec7528169215958f8a21abb339308277ab
Matt Willy <matt.willy@gmail.com>
2022-02-10 09:39:09 -0700

    create a union type and query to support a search

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -9,45 +9,16 @@
 defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
 
-  alias PlateSlateWeb.Resolvers
+  import_types __MODULE__.MenuTypes
 
   query do
-    @desc "The list of available items on the menu"
-    field :menu_items, list_of(:menu_item) do
-      arg :filter, non_null(:menu_item_filter)
-      arg :order, :sort_order, default_value: :asc
-      resolve &Resolvers.Menu.menu_items/3
-    end
-  end
-
-  object :menu_item do
-    field :id, :id
-    field :name, :string
-    field :description, :string
-    field :added_on, :date
+    import_fields :menu_queries
+    import_fields :search_query
   end
 
   enum :sort_order do
     value :asc
     value :desc
-  end
-
-  @desc "Filtering options for the menu item list"
-  input_object :menu_item_filter do
-    @desc "Matching a name"
-    field :name, :string
-    @desc "Matching a category name"
-    field :category, :string
-    @desc "Matching a tag"
-    field :tag, :string
-    @desc "Priced above a value"
-    field :tag, :string
-    @desc "Priced below a value"
-    field :priced_below, :float
-    @desc "Added to the menu before this date"
-    field :added_before, :date
-    @desc "Added to the menu after this date"
-    field :added_after, :date
   end
 
   scalar :date do
---
+++ lib/plate_slate_web/schema/menu_types.ex
@@ -0,0 +1,69 @@
+defmodule PlateSlateWeb.Schema.MenuTypes do
+  use Absinthe.Schema.Notation
+  alias PlateSlateWeb.Resolvers
+
+  @desc "Filtering options for the menu item list"
+  input_object :menu_item_filter do
+    @desc "Matching a name"
+    field :name, :string
+    @desc "Matching a category name"
+    field :category, :string
+    @desc "Matching a tag"
+    field :tag, :string
+    @desc "Priced above a value"
+    field :tag, :string
+    @desc "Priced below a value"
+    field :priced_below, :float
+    @desc "Added to the menu before this date"
+    field :added_before, :date
+    @desc "Added to the menu after this date"
+    field :added_after, :date
+  end
+
+  object :menu_item do
+    field :id, :id
+    field :name, :string
+    field :description, :string
+    field :added_on, :date
+  end
+
+  object :menu_queries do
+    @desc "The list of available items on the menu"
+    field :menu_items, list_of(:menu_item) do
+      arg :filter, non_null(:menu_item_filter)
+      arg :order, :sort_order, default_value: :asc
+      resolve &Resolvers.Menu.menu_items/3
+    end
+  end
+
+  object :category do
+    field :name, :string
+    field :description, :string
+
+    field :items, list_of(:menu_item) do
+      resolve &Resolvers.Menu.items_for_category/3
+    end
+  end
+
+  union :search_result do
+    types [:menu_item, :category]
+
+    resolve_type fn
+      %PlateSlate.Menu.Item{}, _ ->
+        :menu_item
+
+      %PlateSlate.Menu.Category{}, _ ->
+        :category
+
+      _, _ ->
+        nil
+    end
+  end
+
+  object :search_query do
+    field :search, list_of(:search_result) do
+      arg :matching, non_null(:string)
+      resolve &Resolvers.Menu.search/3
+    end
+  end
+end
