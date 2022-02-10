6b29198a3e2dd5e54f450a91b1e2ff2e53d4077b
Matt Willy <matt.willy@gmail.com>
2022-02-10 08:17:48 -0700

    fix warnings and add phoenix formatter

--- lib/plate_slate/application.ex
+++ lib/plate_slate/application.ex
@@ -12,7 +12,6 @@ defmodule PlateSlate.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
-
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
--- lib/plate_slate_web/channels/user_socket.ex
+++ lib/plate_slate_web/channels/user_socket.ex
@@ -12,10 +12,6 @@ defmodule PlateSlateWeb.UserSocket do
   ## Channels
   # channel "room:*", PlateSlateWeb.RoomChannel
 
-  ## Transports
-  transport(:websocket, Phoenix.Transports.WebSocket)
-  # transport :longpoll, Phoenix.Transports.LongPoll
-
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
   # verification, you can put default assigns into
